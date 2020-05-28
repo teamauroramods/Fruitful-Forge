9193661001a26f3618f7f70781fcb1bd353fe658
Epic312 <kaden_ostendorf@yahoo.com>
2020-05-28 14:24:26 -0600

    started work on generation

---
+++ src/main/java/com/epic312/fruitful/common/world/biome/FruitfulBiomeFeatures.java
@@ -0,0 +1,76 @@
+package com.epic312.fruitful.common.world.biome;
+
+import com.epic312.fruitful.core.registry.FruitfulBlocks;
+import com.google.common.collect.ImmutableList;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.blockstateprovider.WeightedBlockStateProvider;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.foliageplacer.BlobFoliagePlacer;
+import net.minecraft.world.gen.treedecorator.BeehiveTreeDecorator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FruitfulBiomeFeatures {
+    public static BlockState OAK_LOG = Blocks.OAK_LOG.getDefaultState();
+    public static BlockState OAK_LEAVES = Blocks.OAK_LEAVES.getDefaultState();
+    public static BlockState APPLE_OAK_LEAVES = FruitfulBlocks.APPLE_OAK_LEAVES.get().getDefaultState();
+
+    public static final TreeFeatureConfig APPLE_OAK_TREE_CONFIG = (
+            new TreeFeatureConfig.Builder(
+                    new SimpleBlockStateProvider(OAK_LOG),
+                    (new WeightedBlockStateProvider()).func_227407_a_(OAK_LEAVES,199).func_227407_a_(APPLE_OAK_LEAVES,1),
+                    new BlobFoliagePlacer(2, 0))).baseHeight(4).heightRandA(2).foliageHeight(3).ignoreVines().setSapling((net.minecraftforge.common.IPlantable) Blocks.OAK_SAPLING)
+            .build();
+
+    public static final TreeFeatureConfig APPLE_OAK_TREE_WITH_MORE_BEEHIVES_CONFIG = (
+            new TreeFeatureConfig.Builder(
+                    new SimpleBlockStateProvider(OAK_LOG),
+                    (new WeightedBlockStateProvider()).func_227407_a_(OAK_LEAVES,199).func_227407_a_(APPLE_OAK_LEAVES,1),
+                    new BlobFoliagePlacer(2, 0))).baseHeight(4).heightRandA(2).foliageHeight(3).ignoreVines().decorators(ImmutableList.of(new BeehiveTreeDecorator(0.05F))).setSapling((net.minecraftforge.common.IPlantable)Blocks.OAK_SAPLING)
+            .build();
+
+    public static void addFruitTrees(Biome biome) {
+        List<ConfiguredFeature<?, ?>> list = biome.getFeatures(GenerationStage.Decoration.VEGETAL_DECORATION);
+        List<ConfiguredFeature<?, ?>> toRemove = new ArrayList<>();
+        int listSize = list.size();
+        for (int i=0; i<listSize; i++) {
+            ConfiguredFeature<?, ?> configuredFeature = list.get(i);
+            if (configuredFeature.config instanceof DecoratedFeatureConfig) {
+                DecoratedFeatureConfig decorated = (DecoratedFeatureConfig) configuredFeature.config;
+                if (decorated.feature.config instanceof MultipleRandomFeatureConfig) {
+                    MultipleRandomFeatureConfig tree = (MultipleRandomFeatureConfig) decorated.feature.config;
+                    List<ConfiguredRandomFeatureList<?>> tempFeatures = new ArrayList<>();
+                    for (ConfiguredRandomFeatureList crfl : tree.features) {
+                        if (crfl.feature.feature instanceof TreeFeature) {
+                            if (crfl.feature.config == DefaultBiomeFeatures.OAK_TREE_CONFIG) {
+                                tempFeatures.add(new ConfiguredRandomFeatureList<TreeFeatureConfig>(Feature.NORMAL_TREE.withConfiguration(APPLE_OAK_TREE_CONFIG), crfl.chance));
+                            } else if (crfl.feature.config == DefaultBiomeFeatures.OAK_TREE_WITH_MORE_BEEHIVES_CONFIG) {
+                                tempFeatures.add(new ConfiguredRandomFeatureList<TreeFeatureConfig>(Feature.NORMAL_TREE.withConfiguration(APPLE_OAK_TREE_WITH_MORE_BEEHIVES_CONFIG), crfl.chance));
+                            }
+                        } else {
+                            tempFeatures.add(crfl);
+                        }
+                    }
+                    ConfiguredFeature<DecoratedFeatureConfig, ?> tempFeature = new ConfiguredFeature<DecoratedFeatureConfig, DecoratedFeature>(
+                            (DecoratedFeature)configuredFeature.feature, new DecoratedFeatureConfig(
+                                new ConfiguredFeature<MultipleRandomFeatureConfig, Feature<MultipleRandomFeatureConfig>>((Feature<MultipleRandomFeatureConfig>)decorated.feature.feature,
+                                        new MultipleRandomFeatureConfig(tempFeatures, tree.defaultFeature)
+                                ), decorated.decorator
+                    ));
+                    toRemove.add(configuredFeature);
+                    biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, tempFeature);
+                }
+            }
+        }
+        for (int i=0; i<toRemove.size(); i++) {
+            list.remove(toRemove.get(i));
+        }
+    }
+}
---
+++ src/main/java/com/epic312/fruitful/common/world/biome/features/FruitTreeFeature.java
@@ -0,0 +1,4 @@
+package com.epic312.fruitful.common.world.biome.features;
+
+public class FruitTreeFeature {
+}
