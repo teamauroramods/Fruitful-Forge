8a539e391b7c7899fb91c0b3b1a9e83c5010336c
Epic312 <kaden_ostendorf@yahoo.com>
2020-07-21 18:52:12 -0600

    you wouldn't eat a bowl

--- src/main/java/com/epic312/fruitful/core/registry/FruitfulItems.java
+++ src/main/java/com/epic312/fruitful/core/registry/FruitfulItems.java
@@ -4,6 +4,7 @@ import com.epic312.fruitful.core.Fruitful;
 import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SoupItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 
@@ -13,5 +14,5 @@ public class FruitfulItems {
 
     public static final RegistryObject<Item> APPLE_PIE = HELPER.createItem("apple_pie", ()->new Item(new Item.Properties().food(FruitfulFoods.APPLE_PIE).group(ItemGroup.FOOD)));
     public static final RegistryObject<Item> PEACH = HELPER.createItem("peach", ()->new Item(new Item.Properties().food(FruitfulFoods.PEACH).group(ItemGroup.FOOD)));
-    public static final RegistryObject<Item> PEACH_COBBLER = HELPER.createItem("peach_cobbler", ()->new Item(new Item.Properties().food(FruitfulFoods.PEACH_COBBLER).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> PEACH_COBBLER = HELPER.createItem("peach_cobbler", ()->new SoupItem(new Item.Properties().food(FruitfulFoods.PEACH_COBBLER).group(ItemGroup.FOOD)));
 }
