3ce71390c50c62d757e90fac1530c32fc6c3b243
Epic312 <kaden_ostendorf@yahoo.com>
2020-09-01 12:24:45 -0600

    made apple crumble, peach cobbler, and orange juice stack to 16 might revert OJ change if it ends up making the food too underpowered

--- src/main/java/com/teamaurora/fruitful/core/registry/FruitfulItems.java
+++ src/main/java/com/teamaurora/fruitful/core/registry/FruitfulItems.java
@@ -13,9 +13,9 @@ import net.minecraftforge.fml.common.Mod;
 public class FruitfulItems {
     public static final RegistryHelper HELPER = Fruitful.REGISTRY_HELPER;
 
-    public static final RegistryObject<Item> APPLE_CRUMBLE = HELPER.createItem("apple_crumble", ()->new SoupItem(new Item.Properties().food(FruitfulFoods.APPLE_CRUMBLE).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> APPLE_CRUMBLE = HELPER.createItem("apple_crumble", ()->new SoupItem(new Item.Properties().food(FruitfulFoods.APPLE_CRUMBLE).group(ItemGroup.FOOD).maxStackSize(16)));
     public static final RegistryObject<Item> PEACH = HELPER.createItem("peach", ()->new Item(new Item.Properties().food(FruitfulFoods.PEACH).group(ItemGroup.FOOD)));
-    public static final RegistryObject<Item> PEACH_COBBLER = HELPER.createItem("peach_cobbler", ()->new SoupItem(new Item.Properties().food(FruitfulFoods.PEACH_COBBLER).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> PEACH_COBBLER = HELPER.createItem("peach_cobbler", ()->new SoupItem(new Item.Properties().food(FruitfulFoods.PEACH_COBBLER).group(ItemGroup.FOOD).maxStackSize(16)));
     public static final RegistryObject<Item> ORANGE = HELPER.createItem("orange", ()->new Item(new Item.Properties().food(FruitfulFoods.ORANGE).group(ItemGroup.FOOD)));
-    public static final RegistryObject<Item> ORANGE_JUICE = HELPER.createItem("orange_juice", ()->new OrangeJuiceItem(new Item.Properties().food(FruitfulFoods.ORANGE_JUICE).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> ORANGE_JUICE = HELPER.createItem("orange_juice", ()->new OrangeJuiceItem(new Item.Properties().food(FruitfulFoods.ORANGE_JUICE).group(ItemGroup.FOOD).maxStackSize(16)));
 }
