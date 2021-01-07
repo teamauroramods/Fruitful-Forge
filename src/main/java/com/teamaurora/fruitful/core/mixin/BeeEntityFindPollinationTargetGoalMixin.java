deadca6385401b5e629650a4a41fa81950c0446f
Sarinsa <mikkemusbruker@gmail.com>
2021-01-07 17:52:36 +0100

    Mostly fixed Epic's headache!

--- src/main/java/com/teamaurora/fruitful/core/mixin/BeeEntityFindPollinationTargetGoalMixin.java
+++ src/main/java/com/teamaurora/fruitful/core/mixin/BeeEntityFindPollinationTargetGoalMixin.java
@@ -1,17 +1,12 @@
 package com.teamaurora.fruitful.core.mixin;
 
-import com.teamaurora.fruitful.common.block.OakBlossomBlock;
 import com.teamaurora.fruitful.core.registry.FruitfulBlocks;
-import net.minecraft.block.*;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.IAngerable;
-import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.LeavesBlock;
 import net.minecraft.entity.passive.BeeEntity;
-import net.minecraft.entity.passive.IFlyingAnimal;
-import net.minecraft.state.IntegerProperty;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,34 +15,30 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BeeEntity.FindPollinationTargetGoal.class)
 public abstract class BeeEntityFindPollinationTargetGoalMixin {
 
+    @Dynamic(value = "This is from a Fruitful mixin :0")
     private BeeEntity b;
 
     private BeeEntityFindPollinationTargetGoalMixin() {
     }
 
-    @Inject(at = @At(value = "TAIL"), method = "<init>")
-    private void registerBee(BeeEntity bee, CallbackInfo ci) {
-        b = bee;
+    @Inject(method = "<init>", at = @At(value = "TAIL"))
+    private void onConstructed(BeeEntity outer, CallbackInfo ci) {
+        b = outer;
     }
 
-    @Inject(at = @At(value = "TAIL"), method = "tick")
-    public void pollinateOakBlossoms(CallbackInfo ci) {
-        if (b.getRNG().nextInt(30) == 0) {
-            for(int i = 1; i <= 2; ++i) {
-                BlockPos blockpos = b.getPosition().down(i);
-                BlockState blockstate = b.world.getBlockState(blockpos);
+    @Inject(method = "tick", at = @At(value = "HEAD"))
+    public void onTick(CallbackInfo ci) {
+        if (b.getRNG().nextInt(15) == 0) {
+            for (BlockPos pos : BlockPos.getAllInBoxMutable(b.getPosition().add(1, 1, 1), b.getPosition().add(-1, -1, -1))) {
+                BlockState blockstate = b.world.getBlockState(pos);
                 Block block = blockstate.getBlock();
-                boolean flag = false;
-                IntegerProperty integerproperty = null;
-                if (block.isIn(BlockTags.BEE_GROWABLES)) {
-                    if (block == FruitfulBlocks.BLOSSOMING_OAK_LEAVES.get() && !blockstate.get(LeavesBlock.PERSISTENT)) {
-                        b.world.playEvent(2005, blockpos, 0);
-                        b.world.setBlockState(blockpos, FruitfulBlocks.APPLE_OAK_LEAVES.get().getDefaultState().with(LeavesBlock.DISTANCE, blockstate.get(LeavesBlock.DISTANCE)));
-                        b.addCropCounter();
-                    }
+
+                if (block == FruitfulBlocks.BLOSSOMING_OAK_LEAVES.get() && !blockstate.get(LeavesBlock.PERSISTENT)) {
+                    b.world.playEvent(2005, pos, 0);
+                    b.world.setBlockState(pos, FruitfulBlocks.APPLE_OAK_LEAVES.get().getDefaultState().with(LeavesBlock.DISTANCE, blockstate.get(LeavesBlock.DISTANCE)));
+                    b.addCropCounter();
                 }
             }
-
         }
     }
 }
